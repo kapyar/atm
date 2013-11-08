cb594f4d6cfc08bb9e9e6b4dfd0f12a0a65be6ac
yaroslav_kaplunsiy <123kapyar@gmail.com>
2013-11-08 02:04:33 +0200

    Add new class MainFormClient

---
+++ src/GUIClient/ClientGUI.java
@@ -0,0 +1,18 @@
+package GUIClient;
+
+import java.io.IOException;
+
+public class ClientGUI {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		MainForm m = new MainForm();
+		m.setVisible(true);
+
+	}
+
+}
