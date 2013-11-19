87b6fac21d00e7cd8e58d1f23e130213bce425cc
yaroslav_kaplunsiy <123kapyar@gmail.com>
2013-11-20 00:22:14 +0200

    Tranfer

--- src/MYGUI/NumbersWithTextField.java
+++ src/MYGUI/NumbersWithTextField.java
@@ -1,11 +1,31 @@
 package MYGUI;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class NumbersWithTextField extends Numbers {
 	private MetroEditablePane textView;
 
 	public NumbersWithTextField() {
 
 		init();
+		addInnerListener();
+	}
+//Outer ENTER AND CANCEL
+	public void addOuterListener(ActionListener a){
+		getMyButton_Cancel().addActionListener(a);
+		getMyButton_Enter().addActionListener(a);
+	}
+	
+	private void addInnerListener() {
+		// this is not inner
+		getListOfComponents().remove(getMyButton_Cancel());
+		getListOfComponents().remove(getMyButton_Enter());
+
+		for (MyButton a : getListOfComponents()) {
+			a.addActionListener(new InnerListener());
+		}
+
 	}
 
 	private void init() {
@@ -27,4 +47,51 @@ public class NumbersWithTextField extends Numbers {
 
 	}
 
-}
+	private class InnerListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+
+			if (source == getMyButton_0()) {
+				writeIt("0");
+			}
+			if (source == getMyButton_1()) {
+				writeIt("1");
+			}
+			if (source == getMyButton_2()) {
+				writeIt("2");
+			}
+			if (source == getMyButton_3()) {
+				writeIt("3");
+			}
+			if (source == getMyButton_4()) {
+				writeIt("4");
+			}
+			if (source == getMyButton_5()) {
+				writeIt("5");
+			}
+			if (source == getMyButton_6()) {
+				writeIt("6");
+			}
+			if (source == getMyButton_7()) {
+				writeIt("7");
+			}
+			if (source == getMyButton_8()) {
+				writeIt("8");
+			}
+			if (source == getMyButton_9()) {
+				writeIt("9");
+			}
+
+		}
+
+		private void writeIt(String s) {
+			textView.getTextField().setText(
+					textView.getTextField().getText() + s);
+		}
+
+	}
+
+}// END InnerListener
+
