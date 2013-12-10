a2d8a39282011194826c02f4772c7957ccdbc317
VladM <webmonch@gmail.com>
2013-12-11 00:58:44 +0200

    Night...

--- src/sms/DataBase.java
+++ src/sms/DataBase.java
@@ -3,7 +3,6 @@ package sms;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
